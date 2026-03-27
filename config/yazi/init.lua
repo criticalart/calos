1f66d2ef481b7aed15c1e4d83126a81ecdc69e3a
criticalart <heels.rower.7z@icloud.com>
2026-03-26 22:36:19 -0700

    yazi upgrade

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -7,7 +7,7 @@ require("bunny"):setup({
 		{ key = "b", path = "/boot", desc = "Boot" },
 		{ key = "c", path = "~/.config", desc = "Config" },
 		{ key = "t", path = "~/.local/share/calos/themes/", desc = "Themes" },
-		{ key = "g", path = "~/.local/share/Steam/steamapps/common", desc = "Steam Games" },
+		{ key = "p", path = "~/Pictures", desc = "Pictures" },
 		{ key = "d", path = "~/Downloads", desc = "Downloads" },
 		-- key and path attributes are required, desc is optional
 	},
@@ -20,3 +20,35 @@ require("bunny"):setup({
 
 -- full-border
 require("full-border"):setup()
+
+require("eza-preview"):setup({
+	-- Set the tree preview to be default (default: true)
+	default_tree = true,
+
+	-- Directory depth level for tree preview (default: 3)
+	level = 1,
+
+	-- Show file icons
+	icons = true,
+
+	-- Follow symlinks when previewing directories (default: true)
+	follow_symlinks = true,
+
+	-- Show target file info instead of symlink info (default: false)
+	dereference = false,
+
+	-- Show hidden files (default: true)
+	all = true,
+
+	-- Ignore files matching patterns (default: {})
+	-- ignore_glob = "*.log"
+	-- ignore_glob = { "*.tmp", "node_modules", ".git", ".DS_Store" }
+	-- SEE: https://www.linuxjournal.com/content/pattern-matching-bash to learn about glob patterns
+	ignore_glob = {},
+
+	-- Ignore files mentioned in '.gitignore'  (default: true)
+	git_ignore = true,
+
+	-- Show git status (default: false)
+	git_status = false,
+})
