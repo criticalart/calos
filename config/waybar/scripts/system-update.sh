7a6eb5027e06cbec7876ac23a426a4cdb7fd34ba
criticalart <heels.rower.7z@icloud.com>
2026-06-16 16:28:53 -0700

    AUR removal from update script + menu tweaks

--- config/waybar/scripts/system-update.sh
+++ config/waybar/scripts/system-update.sh
@@ -1,64 +1,56 @@
 #!/usr/bin/env bash
-#
-# Update system packages using pacman and AUR helper
-#
+
 # Author: Jesse Mirabel <github.com/sejjy>
 # Created: August 16, 2025
 # License: MIT
+# Updates by criticalart include kernel detection, aur removal and misc fixes
 
 GRN='\033[1;32m'
 BLU='\033[1;34m'
 RST='\033[0m'
 
 TIMEOUT=5
-HELPER=$(command -v yay trizen pikaur paru pakku pacaur aurman aura |
-  head -n 1 | xargs -- basename)
 
 check-updates() {
   repo=$(timeout $TIMEOUT pacman -Quq | wc -l) || repo=0
 
-  aur=0
-  if [[ -n $HELPER ]]; then
-    aur=$(timeout $TIMEOUT "$HELPER" -Quaq 2>/dev/null | wc -l) ||
-      aur=0
-  fi
 }
 
 update-packages() {
-  if ((repo + aur == 0)); then
+  if ((repo == 0)); then
     gum spin --spinner "pulse" --spinner.foreground="4" --padding="1 0" --title "No updates found. Press any key to exit..." -- bash -c 'read -n 1 -s'
   else
     if ((repo > 0)); then
       printf '\n%bPackages pending updates:%b\n' "$BLU" "$RST"
       echo
       pacman -Qu
+      local kernel_updated=0
+      if pacman -Qu | grep -q -E "^(linux|linux-zen) "; then
+        kernel_updated=1
+      fi
       gum confirm --padding="1 3" --selected.foreground="0" --prompt.foreground="4" --selected.background="2" "Perform system update?" && sudo pacman -Syu --noconfirm || exit
     fi
 
-    if ((aur > 0)); then
-      printf '\n%bAUR packages pending updates:%b\n' "$BLU" "$RST"
-      echo
-      paru -Qu
-      echo
-      gum confirm --padding="1 3" --selected.foreground="0" --prompt.foreground="4" --selected.background="2" "Update AUR packages?" && paru -Syu --noconfirm --skipreview --removemake || exit
+    if ((kernel_updated == 1)); then
+      gum spin --spinner "pulse" --spinner.foreground="1" --padding="4 0" --title " Kernel updated! Please reboot system to apply changes. Press any key to exit..." -- bash -c 'read -n 1 -s'
+    else
+      gum spin --spinner "pulse" --spinner.foreground="4" --padding="4 0" --title "Update complete! Press any key to exit..." -- bash -c 'read -n 1 -s'
     fi
-
-    notify-send 'Update Complete' -i 'package-install'
-
-    gum spin --spinner "pulse" --spinner.foreground="4" --padding="4 0" --title "Update complete! Press any key to exit..." -- bash -c 'read -n 1 -s'
+    pkill -SIGUSR2 waybar
   fi
 }
 
 display-tooltip() {
-  local tooltip="Official: $repo"
-  if [[ -n $HELPER ]]; then
-    tooltip+="\nAUR($HELPER): $aur"
-  fi
+  local tooltip="Pending Packages: $repo"
 
-  if ((repo + aur == 0)); then
-    echo "{ \"text\": \"󰸟\", \"tooltip\": \"No updates available\" }"
+  if ((repo == 0)); then
+    echo "{ \"text\": \"󰸟\", \"tooltip\": \"System Up to Date\" }"
+  elif pacman -Qu | grep -q -E "^(linux|linux-zen) "; then
+    echo "{ \"text\": \"󰚰 \", \"tooltip\": \"$tooltip\\nKernel Update Available\" }"
+  elif pacman -Qu | grep -q -E "hyprland"; then
+    echo "{ \"text\": \"󱔅\", \"tooltip\": \"$tooltip\\nHyprland Update Available\" }"
   else
-    echo "{ \"text\": \"\", \"tooltip\": \"$tooltip\" }"
+    echo "{ \"text\": \"󰇚\", \"tooltip\": \"$tooltip\" }"
   fi
 }
 
