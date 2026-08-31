efc759d743a95ebbaa28b29554fa94d647c049ae
criticalart <heels.rower.7z@icloud.com>
2026-08-31 00:31:30 -0700

    Clean up theme-update.sh script
    Removed unnecessary instructions and prompts from the theme update script.

--- themes/theme-update.sh
+++ themes/theme-update.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-gum style --border normal --border-foreground="4" --padding="1 3" "This script will $(gum style --bold --foreground 212 'delete') your current theme cache, link your theme folders and regenerate the cache." " " "You should run this if you encounter any issues with the theme selector or if you have added a new theme." "When installing new themes $(gum style --foreground 212 'you must make sure the layout is exactly the same') between themes." "Look at the current theme folder to explore the correct layout." "" "Make sure to delete old symlinks from ~/.config/calos/themes/ !"
-
-gum confirm --padding="1 3" --selected.foreground="0" --prompt.foreground="4" --selected.background="2" "Would you like to execute the script?" && clear || exit 1
 echo "Deleting cache..."
 sleep 0.2
 rm ~/.cache/quickshell/themes/*
