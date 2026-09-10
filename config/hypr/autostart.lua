f4bb5482ee79e1e8be1c84ddac666bc71dd5d335
criticalart <heels.rower.7z@icloud.com>
2026-09-09 18:02:42 -0700

    Modify application autostart commands
    Updated autostart commands for steam and vesktop applications.

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -38,6 +38,6 @@ hl.on("hyprland.start", function()
   )
 
   -- Application autostart
-  --hl.exec_cmd("LD_PRELOAD=/usr/lib32/libextest.so steam", { workspace = "3 silent" })
-  --hl.exec_cmd("uwsm-app -- vesktop", { workspace = "3 silent" })
+        -- hl.exec_cmd("uwsm-app -- steam", { workspace = "special:steamcord silent" })
+        -- hl.exec_cmd("uwsm-app -- vesktop", { workspace = "special:steamcord silent" })
 --end)
